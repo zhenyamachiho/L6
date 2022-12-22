fe62c2f27a8f5f8af7807120ef65ad79833d04f1
Zhenya <zhenya.machiho1@gmail.com>
2022-12-22 20:39:31 +0300

    1

---
+++ L6.1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+
---
+++ L6.2/Program.cs
@@ -0,0 +1,32 @@
+﻿double GetNumber(string message)
+{
+    double result;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (double.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+
+double b1 = GetNumber("Введите b1: ");
+double k1 = GetNumber("Введите k1: ");
+double b2 = GetNumber("Введите b2: ");
+double k2 = GetNumber("Введите k2: ");
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
